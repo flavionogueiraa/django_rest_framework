1ed9f7b3b02862dc339fb89a413d5b6f08c885da
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-05-16 06:31:30 -0300

    Nested serializers

--- core/api/serializers/ponto_turistico_serializer.py
+++ core/api/serializers/ponto_turistico_serializer.py
@@ -1,8 +1,17 @@
+from atracoes.api.serializers import AtracaoSerializer
+from avaliacoes.api.serializers import AvaliacaoSerializer
+from comentarios.api.serializers import ComentarioSerializer
 from core.models import PontoTuristico
+from enderecos.api.serializers import EnderecoSerializer
 from rest_framework import serializers
 
 
 class PontoTuristicoSerializer(serializers.ModelSerializer):
+    endereco = EnderecoSerializer()
+    atracoes = AtracaoSerializer(many=True)
+    comentarios = ComentarioSerializer(many=True)
+    avaliacoes = AvaliacaoSerializer(many=True)
+
     class Meta:
         model = PontoTuristico
         fields = [
@@ -10,4 +19,9 @@ class PontoTuristicoSerializer(serializers.ModelSerializer):
             'nome',
             'descricao',
             'foto',
+            'endereco',
+            'aprovado',
+            'atracoes',
+            'comentarios',
+            'avaliacoes',
         ]
